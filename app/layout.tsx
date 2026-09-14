0b5dfc8f626c91fa447e2a351209824224664f90
Senira Mendis <seniramendis41@gmail.com>
2026-09-14 14:40:45 +0530

    added a new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,19 @@ export const metadata: Metadata = {
   keywords: SEO_KEYWORDS,
   authors: [{ name: 'Senira Mendis', url: SITE_URL }],
   creator: 'Senira Mendis',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    shortcut: ['/favicon.ico'],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
